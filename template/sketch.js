d3120f8bb6194a38c3b7a41274ff9bf6bc5cf1f0
Dan Schellenberg <227656+schellenberg@users.noreply.github.com>
2022-08-29 15:45:28 -0600

    updated sketch.js template and readme
    Making a cs30 specific version of this extension. It would probably be better to just create a custom template that can be chosen when a new project is created, but this is a simple way to make it work for my classes for now.

--- template/sketch.js
+++ template/sketch.js
@@ -1,5 +1,13 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
